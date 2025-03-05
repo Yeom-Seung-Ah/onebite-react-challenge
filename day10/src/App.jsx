d49287b9cfe4a0221881a2abe42ef9025caf79c2
Kenco <75061562+Yeom-Seung-Ah@users.noreply.github.com>
2025-03-05 16:50:36 +0900

    day10

---
+++ day10/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <>
+      <Welcome name={"변우석"} isMember={true} />
+    </>
+  );
+}
+
+export default App;
